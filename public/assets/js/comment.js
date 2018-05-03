7e41b71299d54792cae4835ed9dc5c2392af76c9
unknown <tberry6@gmail.com>
2018-05-03 15:58:24 -0700

    Now allows commenting

---
+++ public/assets/js/comment.js
@@ -0,0 +1,19 @@
+$(function () {
+    $("#comment-form").submit(function (event) {
+        event.preventDefault();
+        var comment = $("textarea#comment-text").val().trim();
+        var id = $(this).data("id");
+        var body = {
+            comment: comment,
+            id: id
+        };
+        $.ajax("/api/comments", {type: "POST", data: body}).then(function (data) {
+            console.log("HIHIHI");
+            location.reload();
+        });
+    });
+
+    $("#reload").on("click", function () {
+        location.reload();
+    })
+});
--- server.js
+++ server.js
@@ -3,13 +3,17 @@ var request = require("request");
 var cheerio = require("cheerio");
 var express = require("express");
 var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
 
 var app = express();
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 var PORT = process.env.PORT || 7000;
 
@@ -24,24 +28,48 @@ mongoose.connect(MONGODB_URI);
 const Schema = mongoose.Schema,
     ObjectId = Schema.ObjectId;
 
+const comment = new Schema({
+    id: ObjectId,
+    text: String
+});
+
 const article = new Schema({
     id: ObjectId,
     title: String,
     summary: String,
-    link: String
+    link: String,
+    comments: [comment]
 });
+
+
 
 const Article = mongoose.model('Article', article);
 
+
+
 app.get("/", function (req, res) {
     Article.find({}, function (error, docs) {
-        res.render("index", {docs: docs});
+        res.render("index", { docs: docs });
     });
 });
 
 app.get("/articles/:id", function (req, res) {
-    Article.find({_id: req.params.id}, function (error, docs) {
+    Article.find({ _id: req.params.id }, function (error, docs) {
         res.render("article", docs[0]);
+    });
+});
+
+app.post("/api/comments", function (req, res) {
+    var id = req.body.id;
+    var text = req.body.comment;
+    console.log(id);
+    Article.find({ _id: id }, function (error, docs) {
+        console.log(docs[0]);
+        docs[0].comments.push({text: text});
+        docs[0].save(function (err) {
+            if (!err) console.log("Success! Added comment: " + text);
+            res.end();
+        });
     });
 });
 
@@ -54,19 +82,18 @@ app.post("/api/scrape", function (req, res) {
             links.push($(element).attr("href"));
         });
         console.log(links.length);
-        scrapeArticle(links, 0);
-        res.end();
+        scrapeArticle(links, 0, res);
     });
 });
 
-function scrapeArticle(links, i) {
+function scrapeArticle(links, i, res) {
     console.log(i);
-    if (i === links.length) return;
+    if (i === links.length) res.end();
     else {
         Article.find({ link: links[i] }, function (error, docs) {
             if (docs.length > 0) {
                 console.log("Article already in database!");
-                scrapeArticle(links, i + 1);
+                scrapeArticle(links, i + 1, res);
             }
             else {
                 request(links[i], function (error, response, html) {
@@ -80,7 +107,7 @@ function scrapeArticle(links, i) {
                     newArticle.save(function (err) {
                         if (!err) console.log(`Success! Added ${title}`);
                     })
-                    scrapeArticle(links, i + 1);
+                    scrapeArticle(links, i + 1, res);
                 });
             }
         });
